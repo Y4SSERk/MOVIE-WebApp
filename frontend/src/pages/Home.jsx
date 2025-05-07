b3891248f26987f5d4682ced3b34c99942f2f815
YASSER <yasser.khattach@gmail.com>
2025-05-07 17:32:33 +0100

    MovieCard component

---
+++ frontend/src/pages/Home.jsx
@@ -0,0 +1,39 @@
+import MovieCard from "../components/MovieCard";
+
+function Home() {
+    const movies = [
+        { id: 1, title: "Movie 1", description: "Description 1" },
+        { id: 2, title: "Movie 2", description: "Description 2" },
+        { id: 3, title: "Movie 3", description: "Description 3" },
+        { id: 4, title: "Movie 4", description: "Description 4" },
+        { id: 5, title: "Movie 5", description: "Description 5" },
+        { id: 6, title: "Movie 6", description: "Description 6" },
+        { id: 7, title: "Movie 7", description: "Description 7" },
+        { id: 8, title: "Movie 8", description: "Description 8" },
+        { id: 9, title: "Movie 9", description: "Description 9" },
+        { id: 10, title: "Movie 10", description: "Description 10" }
+    ]
+
+    const handleSearch = (event) => {
+
+    }
+
+    return <div className="home">
+        <form onSubmit={handleSearch} className="search-form">
+            <input 
+                type="text" 
+                placeholder="Search for movies..." 
+                className="search-input"
+            />
+            <button type="submit" className="search-button">Search</button>
+        </form>
+
+        <div className="movie-grid">
+            {movies.map((movie) => (
+                <MovieCard movie={movie} key={movie.id}/>
+            ))}
+        </div>
+    </div>
+}
+
+export default Home;
