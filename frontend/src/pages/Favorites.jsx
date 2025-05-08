c2bb5049d185629236dea432d9b2c635386e0cd5
YASSER <yasser.khattach@gmail.com>
2025-05-08 12:09:36 +0100

    add context

--- frontend/src/pages/Favorites.jsx
+++ frontend/src/pages/Favorites.jsx
@@ -1,6 +1,22 @@
 import "../css/Favorites.css";
+import { useMovieContext } from "../context/MovieContext";
+import MovieCard from "../components/MovieCard";
 
 function Favorites() {
+    const { favorites } = useMovieContext();
+
+    if (favorites) {
+        <div className="favorites">
+            <h2>Your Favorite Movies</h2>
+            <p>Click on the heart icon to add or remove movies from your favorites</p>
+            return <div className="movie-grid">
+                {favorites.map((movie) => (
+                    <MovieCard key={movie.id} movie={movie} />
+                ))}
+            </div>
+        </div>
+    }
+
     return <div className="favorites-empty">
         <h2>No favorite movies yet</h2>
         <p>Start adding movies to your favorites</p>
